f8d5483c8c20cb989dc9211b6faabb3c45f51f45
francesco.cecconi <francesco.cecconi@c1291736-2539-0410-a836-7dc8ed2dc041>
2008-06-23 16:46:35 +0000

    fix Cmake file and created a translation file
    git-svn-id: https://nmapsi4.googlecode.com/svn/trunk/nmapsi4@293 c1291736-2539-0410-a836-7dc8ed2dc041

---
+++ nmapsi4-logr/ts/nmapsi4-logr_fr.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>mWindow</name>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="102"/>
+        <source>Nmapsi4 log reader</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="103"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="106"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="105"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="107"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="108"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="109"/>
+        <source>Host(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="110"/>
+        <source>&amp;FIle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="111"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/ui_mainwin.h" line="112"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mwClass</name>
+    <message>
+        <location filename="../src/logrApi.cpp" line="6"/>
+        <source>Open Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logrApi.cpp" line="7"/>
+        <source>Log (*.log)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
