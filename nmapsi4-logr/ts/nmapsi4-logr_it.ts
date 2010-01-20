d39e430219226dc6716639c4032f4759a776d452
eldiablo <eldiablo@amd64box.home.lan>
2010-01-20 02:32:22 +0100

    Logr Italian translation up-to-date

--- nmapsi4-logr/ts/nmapsi4-logr_it.ts
+++ nmapsi4-logr/ts/nmapsi4-logr_it.ts
@@ -1,84 +1,84 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="it_IT">
 <context>
     <name>mWindow</name>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="197"/>
         <source>Host(s)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="202"/>
         <source>Log History</source>
-        <translation type="unfinished"></translation>
+        <translation>Cronologia Log</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="187"/>
         <source>Nmapsi4 log reader</source>
-        <translation type="unfinished"></translation>
+        <translation>Lettore log Nmapsi4</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="188"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Pulisci</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="189"/>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="191"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Esci</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="190"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Apri</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="192"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="193"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="194"/>
         <source>About &amp;Qt</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="196"/>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="201"/>
         <source>List(s)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="203"/>
         <source>&amp;FIle</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;FIle</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="204"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aiuto</translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="205"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="199"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../build/nmapsi4-logr/ui_mainwin.h" line="200"/>
         <source>Path</source>
-        <translation type="unfinished"></translation>
+        <translation>Percorso</translation>
     </message>
 </context>
 <context>
@@ -86,12 +86,12 @@
     <message>
         <location filename="../src/logrApi.cpp" line="7"/>
         <source>Open Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Apri log</translation>
     </message>
     <message>
         <location filename="../src/logrApi.cpp" line="8"/>
         <source>Log (*.log)</source>
-        <translation type="unfinished"></translation>
+        <translation>Log (*.log)</translation>
     </message>
 </context>
 </TS>
