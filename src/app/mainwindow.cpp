7217c3d6d157aec9b0f9579183cf2048bf7448e2
Francesco Cecconi <francesco.cecconi@gmail.com>
2012-02-15 00:37:38 +0100

    restore MainWindow destructor.

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -307,6 +307,7 @@ void MainWindow::closeEvent(QCloseEvent * event)
             m_monitor->clearHostMonitor();
             saveSettings();
             event->accept();
+            delete this;
             break;
         case QMessageBox::Cancel:
             event->ignore();
@@ -319,6 +320,7 @@ void MainWindow::closeEvent(QCloseEvent * event)
         // Save Ui settings
         saveSettings();
         event->accept();
+        delete this;
     }
 }
 
