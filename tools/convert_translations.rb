2f36104b09c6693512141909d6e5579e09ceaeb1
Francesco Cecconi <francesco.cecconi@gmail.com>
2012-08-29 18:48:29 +0200

    update translations converter.

--- tools/convert_translations.rb
+++ tools/convert_translations.rb
@@ -26,28 +26,6 @@ def ts2PoNmapsi4
     result = %x[echo; ts2po -P #{path_string_src} #{path_string_dst}]
     puts "Result pot2: #{result}"
 end
-def ts2PoNmapsi4_logr
-    @tmp = %x[echo; ls ../nmapsi4-logr/ts/]
-    for value in @tmp
-	if (value_str = value.eql?("po\n")) == false && (value_str2 = value.eql?("\n")) == false && (value_str3 = value.eql?("template\n")) == false
-	    tmp_clear = value.delete("\n")
-	    tmp_clear = value.chop.chop.chop.chop
-	    tmp_clear = tmp_clear + ".po"
-	    tmp_clear["nmapsi4-logr_"] = ""
-	    path_string_dst = "../nmapsi4-logr/ts/po/nmapsi4-logr/"+tmp_clear
-	    path_string_src = "../nmapsi4-logr/ts/"+value
-	    path_string_src = path_string_src.delete("\n")
-	    path_string_dst = path_string_dst.delete("\n")
-	    result = %x[echo; ts2po #{path_string_src} #{path_string_dst}]
-	    puts "Result: #{result}"
-	end
-    end
-    # TODO generic template pot
-    path_string_dst = "../nmapsi4-logr/ts/po/nmapsi4-logr/nmapsi4-logr.pot"
-    path_string_src = "../nmapsi4-logr/ts/template/nmapsi4-logr.ts"
-    result = %x[echo; ts2po -P #{path_string_src} #{path_string_dst}]
-    puts "Result pot2: #{result}"
-end
 end
 
 
@@ -59,7 +37,6 @@ if __FILE__ == $0
     else if ARGV.first == "convert"
 	    ts = Tools.new
 	    ts.ts2PoNmapsi4
-	    #ts.ts2PoNmapsi4_logr
 	else if ARGV.first == "revert"
 	    ts = Tools.new
 	    # revert metod
