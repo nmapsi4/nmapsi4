ab004cbb6fb129c7e860ed662394c2296b0a9e80
Francesco Cecconi <francesco.cecconi@gmail.com>
2012-09-06 21:22:23 +0200

    remove defaultuisettings file.

--- src/app/settings.cpp
+++ src/app/settings.cpp
@@ -21,7 +21,6 @@
 
 void MainWindow::syncSettings()
 {
-
     QSettings settings("nmapsi4", "nmapsi4");
 
     // TODO: load saved profile or simple default
@@ -88,3 +87,150 @@ void MainWindow::saveSettings()
     }
 #endif
 }
+
+void MainWindow::updateCompleter()
+{
+    if (!m_bookmark->isBookmarkHostListEmpty())
+    {
+        if (!m_completer.isNull())
+        {
+            QStringListModel *newModel = qobject_cast<QStringListModel*>(m_completer.data()->model());
+            newModel->setStringList(m_bookmark->getHostListFromBookmark());
+        }
+        else if (m_hostModel.isNull())
+        {
+            m_hostModel = new QStringListModel(m_bookmark->getHostListFromBookmark(),this);
+        }
+    }
+
+    if (!m_bookmark->isBookmarkServicesListEmpty())
+    {
+        if (!m_completerVuln.isNull())
+        {
+            QStringListModel *newModel = qobject_cast<QStringListModel*>(m_completerVuln.data()->model());
+            newModel->setStringList(m_bookmark->getServicesListFromBookmark());
+        }
+        else if (m_vulnModel.isNull())
+        {
+            m_vulnModel = new QStringListModel(m_bookmark->getServicesListFromBookmark(),this);
+        }
+    }
+}
+
+void MainWindow::restoreSettings()
+{
+    // restore window position
+    QSettings settings("nmapsi4", "nmapsi4");
+    QPoint pos = settings.value("window/pos", QPoint(200, 200)).toPoint();
+    QSize size = settings.value("window/size", QSize(910, 672)).toSize();
+    resize(size);
+    move(pos);
+
+    // restore state of the QAction's connected to splitter widget
+    if (!settings.value("splitterSizes").toByteArray().isEmpty())
+    {
+        m_mainHorizontalSplitter->restoreState(settings.value("splitterSizes").toByteArray());
+
+        if (m_mainHorizontalSplitter->sizes()[0])
+        {
+            m_collections->m_collectionsButton.value("scan-list")->setChecked(true);
+        }
+        else
+        {
+            m_collections->m_collectionsButton.value("scan-list")->setChecked(false);
+        }
+    }
+    else
+    {
+        m_collections->m_collectionsButton.value("scan-list")->setChecked(true);
+    }
+
+    if (!settings.value("splitterSizesRight").toByteArray().isEmpty())
+    {
+        m_mainVerticalSplitter->restoreState(settings.value("splitterSizesRight").toByteArray());
+
+        if (m_mainVerticalSplitter->sizes()[1])
+        {
+            m_collections->m_collectionsButton.value("details-list")->setChecked(true);
+        }
+        else
+        {
+            m_collections->m_collectionsButton.value("details-list")->setChecked(false);
+        }
+    }
+    else
+    {
+        m_collections->m_collectionsButton.value("details-list")->setChecked(true);
+    }
+}
+
+void MainWindow::setTreeSettings()
+{
+    // set TreeWidget properties
+    treeLogH->setColumnWidth(0, 400);
+    treeBookPar->setColumnWidth(0, 400);
+    scanMonitor->setColumnWidth(0, 300);
+    scanMonitor->setColumnWidth(1, 350);
+    //treeTraceroot->setColumnWidth(0, 250);
+    treeTraceroot->setColumnWidth(1, 100);
+    treeTraceroot->setColumnWidth(2, 200);
+    treeTraceroot->setColumnWidth(3, 200);
+    treeBookVuln->setColumnWidth(0, 400);
+    treeWidgetVulnUrl->setColumnWidth(0, 400);
+    treeMain->setColumnWidth(0, 200);
+    treeDiscover->setColumnWidth(0, 300);
+}
+
+void MainWindow::setDefaultAction()
+{
+    // Set default properties
+    action_Scan_menu->setEnabled(false);
+    action_Scan_2->setEnabled(false);
+    actionAdd_Bookmark->setEnabled(false);
+    toolBarSearch->setVisible(false);
+    m_collections->m_collectionsButton.value("scan-sez")->setChecked(true);
+
+    menuBar()->setContextMenuPolicy(Qt::PreventContextMenu);
+    nseNumber->setSegmentStyle(QLCDNumber::Flat);
+    tWresult->setVisible(false);
+
+    tabUi->removeTab(tabUi->indexOf(tabVulnMain));
+    tabUi->removeTab(tabUi->indexOf(tabVulnBookmarks));
+    tabUi->removeTab(tabUi->indexOf(tabDiscover));
+
+    actionAdd_vulnerability_search_url->setVisible(false);
+}
+
+void MainWindow::setDefaultSplitter()
+{
+    // define default Ui splitter
+    m_mainHorizontalSplitter = new QSplitter(this);
+    m_mainVerticalSplitter = new QSplitter(this);
+
+    connect(m_mainVerticalSplitter, SIGNAL(splitterMoved(int,int)),
+            this, SLOT(resizeVerticalSplitterEvent()));
+
+    connect(m_mainHorizontalSplitter, SIGNAL(splitterMoved(int,int)),
+            this, SLOT(resizeHorizontalSplitterEvent()));
+
+    m_mainHorizontalSplitter->setOrientation(Qt::Horizontal);
+    m_mainHorizontalSplitter->addWidget(frameLeft);
+    m_mainHorizontalSplitter->addWidget(frameCenter);
+    //frameCenter
+    m_mainVerticalSplitter->setOrientation(Qt::Vertical);
+    m_mainVerticalSplitter->addWidget(tabWidget);
+    m_mainVerticalSplitter->addWidget(frameRight);
+    // insert splitter
+    tabUi->widget(0)->layout()->addWidget(m_mainHorizontalSplitter);
+    frameCenter->layout()->addWidget(m_mainVerticalSplitter);
+    frameCenter->layout()->addWidget(frame_2);
+}
+
+void MainWindow::defaultComboValues()
+{
+    hostEdit->setStyleSheet(QString::fromUtf8("color: rgb(153, 153, 153);"));
+    hostEdit->insertItem(0, tr("Host(s) to scan (ip/dns or incremental - ex. 192.168.1.1/20)"));
+    comboVulnRis->setStyleSheet(QString::fromUtf8("color: rgb(153, 153, 153);"));
+    comboVulnRis->insertItem(0, tr("Search Vulnerabilities"));
+}
+
