1a9a525c2388fef32b1f64c59000e40c51895a67
Francesco Cecconi <francesco.cecconi@gmail.com>
2012-09-30 11:58:46 +0200

    make bottom toolBar variables private

--- src/app/actionmanager.h
+++ src/app/actionmanager.h
@@ -49,8 +49,6 @@ public:
     QHash<QString, QAction*> m_collectionsDiscover;
     QHash<QString, QAction*> m_collectionsScanSection;
     QToolBar *m_discoverToolBar;
-    QToolBar *m_bottomToolBar;
-    QToolBar *m_bottomScanBar;
 
 private:
     MainWindow* m_ui;
@@ -59,6 +57,8 @@ private:
     QToolButton *m_saveTool;
     QToolButton *m_bookmarksTool;
     QToolButton *m_discoverScanTool;
+    QToolBar *m_bottomToolBar;
+    QToolBar *m_bottomScanBar;
 
 public slots:
     void scanBookmarkContextMenu();
