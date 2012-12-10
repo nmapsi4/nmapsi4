023e30f5f381e4723d4020808d249edb595ad786
Francesco Cecconi <francesco.cecconi@gmail.com>
2012-12-10 20:15:23 +0100

    update copyrights.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007-2011 by Francesco Cecconi                          *
+ *   Copyright (C) 2007-2012 by Francesco Cecconi                          *
  *   francesco.cecconi@gmail.com                                           *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
